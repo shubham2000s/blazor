fb4272ebffe80fda00007bca6269a0649a79ffb7
Shubham Kumawat <shubhamkumawat165@gmail.com>
2025-12-15 15:55:46 +0530

    save work before switching branch

--- frontend-web/src/Recruitment.AdminWeb/obj/Debug/net8.0/Recruitment.AdminWeb.AssemblyInfo.cs
+++ frontend-web/src/Recruitment.AdminWeb/obj/Debug/net8.0/Recruitment.AdminWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recruitment.AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd152b85f8de5427a80cef1044550e3c7be5ebe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a3fb03b712c13d7610016a62d30bdcade81c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recruitment.AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recruitment.AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
